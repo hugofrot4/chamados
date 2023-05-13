fe9af017e83fe317d114093ea6d61e247258f1af
Hugo Frota <herbtyh@gmail.com>
2023-05-13 17:21:25 -0300

    Adicionar página de chamados

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,12 @@ import { AuthContext } from '../../contexts/auth';
 import { useContext } from 'react';
 
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi';
+
+import { Link } from 'react-router-dom';
+
+import './dashboard.css';
 
 export default function Dashboard() {
   const { logout } = useContext(AuthContext);
@@ -13,8 +19,82 @@ export default function Dashboard() {
   return (
     <div>
       <Header />
-      <h1>Página Dashboard</h1>
-      <button onClick={handleLogout}>Sair da conta</button>
+
+      <div className="content">
+        <Title name="Tickets">
+          <FiMessageSquare size={25} />
+        </Title>
+
+        <>
+          <Link to="/new" className="new">
+            <FiPlus color="#fff" size={25} />
+            Novo Chamado
+          </Link>
+
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Cliente</th>
+                <th scope="col">Assunto</th>
+                <th scope="col">Status</th>
+                <th scope="col">Cadastrado em</th>
+                <th scope="col">#</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Cliente">Mercado Esquina</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">
+                  <span className="badge" style={{ backgroundColor: '#999' }}>
+                    Em aberto
+                  </span>
+                </td>
+                <td data-label="Cadastrado">26/03/1999</td>
+                <td data-label="#">
+                  <button
+                    className="action"
+                    style={{ backgroundColor: '#3583f6' }}
+                  >
+                    <FiSearch color="#fff" size={17} />
+                  </button>
+                  <button
+                    className="action"
+                    style={{ backgroundColor: '#f6a935' }}
+                  >
+                    <FiEdit2 color="#fff" size={17} />
+                  </button>
+                </td>
+              </tr>
+
+              <tr>
+                <td data-label="Cliente">Informatica Tech</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">
+                  <span className="badge" style={{ backgroundColor: '#999' }}>
+                    Em aberto
+                  </span>
+                </td>
+                <td data-label="Cadastrado">26/03/1999</td>
+                <td data-label="#">
+                  <button
+                    className="action"
+                    style={{ backgroundColor: '#3583f6' }}
+                  >
+                    <FiSearch color="#fff" size={17} />
+                  </button>
+                  <button
+                    className="action"
+                    style={{ backgroundColor: '#f6a935' }}
+                  >
+                    <FiEdit2 color="#fff" size={17} />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      </div>
     </div>
   );
 }
