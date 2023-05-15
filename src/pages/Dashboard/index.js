7d3140f8eaf0b6680654a1748713adfc2f0c2f99
Hugo Frota <herbtyh@gmail.com>
2023-05-15 15:44:00 -0300

    Adicionar páginação

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -27,7 +27,10 @@ export default function Dashboard() {
 
   const [chamados, setChamados] = useState([]);
   const [loading, setLoading] = useState(true);
+
   const [isEmpty, setIsEmpty] = useState(false);
+  const [lastDocs, setLastDocs] = useState();
+  const [loadingMore, setLoadingMore] = useState(false);
 
   useEffect(() => {
     async function loadChamados() {
@@ -65,10 +68,28 @@ export default function Dashboard() {
         });
       });
 
+      const lastDoc = querySnapshot.docs[querySnapshot.docs.length - 1]; // Pega o ultimo item
+
       setChamados((chamados) => [...chamados, ...lista]);
+      setLastDocs(lastDoc);
     } else {
       setIsEmpty(true);
     }
+
+    setLoadingMore(false);
+  }
+
+  async function handleMore() {
+    setLoadingMore(true);
+
+    const q = query(
+      listRef,
+      orderBy('created', 'desc'),
+      startAfter(lastDocs),
+      limit(5),
+    );
+    const querySnapshot = await getDocs(q);
+    await upadateState(querySnapshot);
   }
 
   if (loading) {
@@ -133,7 +154,10 @@ export default function Dashboard() {
                         <td data-label="Status">
                           <span
                             className="badge"
-                            style={{ backgroundColor: '#999' }}
+                            style={{
+                              backgroundColor:
+                                item.status === 'Aberto' ? '#5cb85c' : '#999',
+                            }}
                           >
                             {item.status}
                           </span>
@@ -158,6 +182,13 @@ export default function Dashboard() {
                   })}
                 </tbody>
               </table>
+
+              {loadingMore && <h3>Buscando mais chamados...</h3>}
+              {!loadingMore && !isEmpty && (
+                <button className="btn-more" onClick={handleMore}>
+                  Buscar mais
+                </button>
+              )}
             </>
           )}
         </>
